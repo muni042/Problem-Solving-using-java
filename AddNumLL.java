c38d38471b1c208bc713ea1e0af0dab5d937c1d6
muni042 <muniselvamselvakumar@gmail.com>
2025-11-22 08:50:04 +0000

    only add two linklist remain

--- AddNumLL.java
+++ AddNumLL.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class LListFuction{
     Node head1,tail1;
+    int size1=0;
+    int size2=0;
     Node head2,tail2;
     public void addnumList1(int a)
     {
@@ -9,11 +11,13 @@ class LListFuction{
         {
             head1=new Node(a);
             tail1=head1;
+            size1++;
         }
         else{
             Node temp=new Node(a);
             tail1.next=temp;
             tail1=temp;
+            size1++;
         }
 
     }
@@ -23,18 +27,26 @@ class LListFuction{
         {
             head2=new Node(a);
             tail1=head1;
+            size2++;
         }
         else{
             Node temp=new Node(a);
             tail2.next=temp;
             tail2=temp;
+            size2++;
         }
 
     }
     public Node AddTwoList(Node head1,Node head2)
     {
         Node head3;
-        
+        if(head1==null && head2==null)
+        {
+            return head3;
+        }
+        else{
+            
+        }
         return head3;
 
     }
